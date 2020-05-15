5ec5fb6bf32c2ac571a3ef00406a0a2cb28e59ca
sayalipawade <sayalipawade05@gmail.com>
2020-05-15 11:52:33 +0530

    Added:Ensure that player gets exact winning position

--- SnakeLadder.js
+++ SnakeLadder.js
@@ -4,32 +4,33 @@ console.log("******Welcome to snake and ladder******")
  * Constants
  */
 let STARTPOSITION=0
+let WINNINGPOSITION=100
+let SNAKE=2
+let LADDER=1
+let NOPLAY=0
 
 /**
  * Variables
  */
 let position_Of_Player=0
-let snake=2
-let ladder=1
-let noPlay=0
 
 /**
  * Player checking for option-no play,snake or ladder
  */
-for(let i=0;i<100;i++)
+while(position_Of_Player<WINNINGPOSITION)
 {
     //Generating random no
     let randomPosition=Math.floor(Math.random()*6+1);
     let option=Math.floor(Math.random()*3+1)
     switch(option)
     {
-        case noPlay:
+        case NOPLAY:
                 position_Of_Player=0
                 break;
-        case ladder:
+        case LADDER:
                 position_Of_Player=position_Of_Player+randomPosition
                 break;
-        case snake:
+        case SNAKE:
                 position_Of_Player=position_Of_Player-randomPosition
                 break;
     }
