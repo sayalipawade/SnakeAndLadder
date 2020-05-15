2d874209bbb601b1ae0e1b1012d5f2a1d640ec25
sayalipawade <sayalipawade05@gmail.com>
2020-05-15 10:42:35 +0530

    Added:Player checking option for no play,snake and ladder

--- SnakeLadder.js
+++ SnakeLadder.js
@@ -5,4 +5,32 @@ console.log("******Welcome to snake and ladder******")
  */
 let STARTPOSITION=0
 
+/**
+ * Variables
+ */
+let position_Of_Player=0
+let snake=2
+let ladder=1
+let noPlay=0
+
+/**
+ * Generating random no
+ */
 let randomPosition=Math.floor(Math.random()*6+1);
+
+/**
+ * Player checking for option-no play,snake or ladder
+ */
+let option=Math.floor(Math.random()*3+1)
+switch(option)
+{
+    case noPlay:
+        position_Of_Player=0
+        break;
+    case ladder:
+        position_Of_Player=position_Of_Player+randomPosition
+        break;
+    case snake:
+        position_Of_Player=position_Of_Player-randomPosition
+        break;
+}
